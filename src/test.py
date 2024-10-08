391128e359996d57ba01af9bc0954ac0b5d6a08b
George Mushore <gmu003@gmail.com>
2024-10-08 20:39:58 +0200

    Re-add src/ directory after accidental untracking

---
+++ src/test.py
@@ -0,0 +1,6 @@
+from matplotlib import pyplot as plt
+from src.plots import plot_accuracy_vs_regularization
+
+if __name__ == '__main__':
+    plot_accuracy_vs_regularization([1, 2, 3], [4, 5, 6], [7, 8, 9]) 
+    plt.savefig("reports/figures/accuracy_vs_regularization.png")
